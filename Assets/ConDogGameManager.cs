fdd45f7f98a3a775f7c459274ea51819d262d01d
localadmin@euro <localadmin@euro>
2018-11-29 15:46:09 +0000

    Some networking

---
+++ Assets/ConDogGameManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Com.MyCompany.MyGame
+{
+    public class ConDogGameManager : Photon.PunBehaviour
+    {
+
+
+        public GameObject ThrowableObjPrefab;
+
+        #region Photon Messages
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.Log("PhotonNetwork : Loading Level : Rocket" );
+            PhotonNetwork.LoadLevel("Rocket");
+        }
+
+
+        #endregion
+
+        #region Photon Messages
+
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerConnected() " + other.NickName); // not seen if you're the player connecting
+
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+                LoadArena();
+            }
+        }
+
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects
+
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerDisonnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+                LoadArena();
+            }
+        }
+
+
+        #endregion
+    }
+
+
+}
---
+++ Assets/SteamVR/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Photon.PunBehaviour
+{
+    
+    #region Private Methods
+
+
+    void LoadArena()
+    {
+        if (!PhotonNetwork.isMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.Log("PhotonNetwork : Loading Level : Rocket");
+        PhotonNetwork.LoadLevel("Rocket");
+    }
+    #endregion
+
+    #region Photon Messages
+
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected() " + other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadArena();
+        }
+    }
+
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerDisonnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadArena();
+        }
+    }
+    
+    #endregion
+}
